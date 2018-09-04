1d59fe4d8b91844d6c0a5adba9ca3ae7396c25f0
ld <liu.dong6@ztesoft.com>
2018-09-04 10:14:22 +0800

    更换http请求

--- src/main/java/com/idea/core/utils/http/HttpRequest.java
+++ src/main/java/com/idea/core/utils/http/HttpRequest.java
@@ -1,9 +1,12 @@
 package com.idea.core.utils.http;
 
 import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -29,6 +32,8 @@ public class HttpRequest {
 	 * @return URL 所代表远程资源的响应结果
 	 * @throws IOException
 	 */
+	 public static final String POST = "POST";
+	 public static final String GET = "GET";
 	public static String sendGet(String url, String param) throws IOException {
 		String result = "";
 		BufferedReader in = null;
@@ -95,41 +100,61 @@ public class HttpRequest {
 	 * @return 所代表远程资源的响应结果
 	 * @throws IOException
 	 */
-	public static String sendPost(String url, String param) throws IOException {
-		PrintWriter out = null;
-		BufferedReader in = null;
-		String result = "";
-		URL realUrl = new URL(url);
-		// 打开和URL之间的连接
-		URLConnection conn = realUrl.openConnection();
-		// 设置通用的请求属性
-		conn.setRequestProperty("accept", "*/*");
-		conn.setRequestProperty("connection", "Keep-Alive");
-		conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-		// 发送POST请求必须设置如下两行
-		conn.setDoOutput(true);
-		conn.setDoInput(true);
-		// 获取URLConnection对象对应的输出流
-		out = new PrintWriter(conn.getOutputStream());
-		// 发送请求参数
-		out.print(param);
-		// flush输出流的缓冲
-		out.flush();
-		// 定义BufferedReader输入流来读取URL的响应
-		in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String line;
-		while ((line = in.readLine()) != null) {
-			result += line;
-		}
-		if (out != null) {
-			out.close();
-		}
-		if (in != null) {
-			in.close();
-		}
-		return result;
-	}
-
+	public static String sendPost(String path,String body) throws IOException {
+		DataOutputStream out = null;
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+            connection.setInstanceFollowRedirects(true);
+            connection.setRequestMethod(POST); 
+            connection.setRequestProperty("Accept", "text/xml,text/javascript,text/html"); 
+            connection.setRequestProperty("Connection", "keep-alive");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"); 
+            connection.connect();
+            if(body != null) {
+                out = new DataOutputStream(connection.getOutputStream());
+                out.write(body.getBytes("utf-8")); 
+                out.flush();
+                out.close();
+            }
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+           String reslut= builder.toString();
+            connection.disconnect(); 
+            return reslut;
+        }
+        catch (Exception e) {
+        	e.printStackTrace();
+            System.out.println("HTTP POST发生异常" + e.getLocalizedMessage());
+        }
+        finally {
+        	close(out);
+        	close(reader);
+        }
+        return null;
+    }
+	//关闭
+    public static void close(Closeable closeable) {
+        if (closeable == null) {
+            return;
+        }
+        try {
+            closeable.close();
+        }
+        catch (Exception e) {
+        	e.printStackTrace();
+            System.out.println("发生异常" + e.getMessage());
+        }
+    }
 	/**
 	 * 向指定 URL 发送POST方法的请求
 	 * 
